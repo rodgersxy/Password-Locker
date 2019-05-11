6eb1a59172d22da358c6350fb50a940c7008017c
rodgers <rodgersny99@gmail.com>
2019-05-11 22:22:41 +0300

    run tests

--- user_test.py
+++ user_test.py
@@ -15,3 +15,17 @@ class TestUser(unittest.TestCase):
         Set up method to run before each test case.
         '''
         self.user_list = User("Rodgers","Nyakundi","0726746234","rodgersny99@gmail.com","password")
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is properly initialized
+        '''
+
+        self.assertEqual(self.user_list.first_name,"Rodgers")
+        self.assertEqual(self.user_list.last_name,"Nyakundi")
+        self.assertEqual(self.user_list.phone_number,"0726746234")
+        self.assertEqual(self.user_list.email,"rodgersny99@gmail.com")
+        self.assertEqual(self.user_list.password,"password")
+if __name__ == '__main__':
+    unittest.main()        
