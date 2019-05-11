fc0135e922a6e0940eb55e49a557deb7f48d6d34
rodgers <rodgersny99@gmail.com>
2019-05-12 01:41:14 +0300

    add search function

--- app.py
+++ app.py
@@ -97,6 +97,66 @@ def main():
             print("New User")
             print("-"*10)
 
+            print ("First name .....")
+            f_name = input()
+
+            print("Last name ....")
+            l_name = input()
+
+            print("Phone number ...")
+            p_number = input()
+
+            print("Email address ...")
+            e_address = input()
+
+            print("Password .....")
+            e_pass = input()
+
+            save_users(create_user(f_name,l_name,p_number,e_address,e-pass))
+
+            print('\n')
+
+            print(f"New User {f_name} {l_name} created")
+
+            print ('\n')
+
+        elif short_code == 'du':
+            if display_users():
+
+                print("Here is a list of all your users")
+
+                print('\n')
+
+                for user in display_users():
+                    print(f"{user.first_name} {user.last_name} .....{user.phone_number}")
+
+                print('\n')
+
+            else:
+
+                print('\n')
+
+                print("You dont seem to have any users saved yet")
+
+                print('\n')
+
+        elif short_code == 'fu':
+
+            print("Enter the number you want to search for")
+
+            search_number = input()
+
+            if check_existing_users(search_number):
+
+                search_user = find_user(search_number)
+
+                print(f"{search_user.first_name} {search_user.last_name}")
+
+                print('-' *20)
+
+                print(f"Phone number...........{search_user.phone_number}")        
+
+
 
 
 
